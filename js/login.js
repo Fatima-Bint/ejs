546033bf2e525f5a2f1e41a1cc10ca9dc7b235bc
Admin <fatimatib44@gmail.com>
2023-10-13 12:37:28 +0000

    first commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.submit');
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+   
+   let url1 = 'http://localhost:7000/';
+   console.log(url)
+   window.location.href = url;
+   
+
+});
